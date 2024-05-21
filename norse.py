4baae441344ffb214f1b35f296b7ed8c8b0fbc48
Jens Egil Haraldset <jens.haraldsete@gmail.com>
2024-05-22 00:03:48 +0200

    Fixed a lot

--- norse.py
+++ norse.py
@@ -7,11 +7,19 @@ from pymongo.server_api import ServerApi
 
 def getFairs(cabins, name):
     for cabin in cabins:
-        if cabin["cabinName"] == "Economy":
+        print("\n\n\n")
+        print(cabin)
+        if cabin["cabinName"] == name:
             return cabin
     return {}
 
 '''
+Database:
+
+Distance: OSL-BKK
+    -> Price data
+
+
 curl 'https://services.flynorse.com/api/v1/token' \
   -H 'authority: services.flynorse.com' \
   -H 'accept: application/json, text/plain, */*' \
@@ -121,40 +129,117 @@ def connect_mongo():
     col = client["norse"]["norsedata"]
     return col
 
-def main():
+def is_token_timeout(payload):
+    if 'errors' in jsn:
+        token_errors = [error for error in jsn['errors'] if error['code'] == 'core:Token:Timeout']
+        if len(token_errors) > 0:
+            print("Token timeout")
+            return True
+    return False
 
+def create_token():
+    ''''''
+    url = "https://services.flynorse.com/api/v1/token"
+    headers = {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        "pragma": "no-cache",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Linux\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "cookie": "ARRAffinity=aa8e1fc94231a6616d9c9cf9d19a69c2456023250690611bf27c09279050ef5d; ARRAffinitySameSite=aa8e1fc94231a6616d9c9cf9d19a69c2456023250690611bf27c09279050ef5d; _ga_LELRJSCXE1=GS1.1.1716305177.1.1.1716305303.60.0.0; _ga_XXXXXXXXXX=GS1.1.1716305147.12.1.1716305303.0.0.126804701; _hjSessionUser_3855766=eyJpZCI6IjdjYjE3NTQwLWIzOWMtNTNlZC1hYTA2LTZkODlhYmVjMTlkOCIsImNyZWF0ZWQiOjE3MTYzMDUzMDQwNjUsImV4aXN0aW5nIjp0cnVlfQ==; _hjSession_3855766=eyJpZCI6ImQ3ZjU3MzcwLTg4OWMtNGI0ZS04NzYyLWRkY2NkMDg3ZTk0OCIsImMiOjE3MTYzMDUzMDQwNjYsInMiOjEsInIiOjEsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjoxLCJzcCI6MH0=; FPID=FPID2.2.sa6z3ZtB%2FonPtDqVw%2Beicfh6MQZAccVU6h0LNh92lTo%3D.1716305177; FPLC=coQYWZVI5a5B45hobmS%2FRLx5alIzKdVyU1rJXYj21eHvNQCb0w8%2BIyMeU%2Bud4NT0bdBDAM6YD0EgyExMOC%2Fe0vVtrPxf3LFiQXSHdxTzE%2BoJBenixZnEYIT7VqZAdw%3D%3D; rl_anonymous_id=RS_ENC_v3_IjkwNzc2MDVjLWU5ZDItNDBjZC04NjNmLWMyMmJkYjM1N2Q4YyI%3D; rl_page_init_referrer=RS_ENC_v3_Imh0dHBzOi8vd3d3Lmdvb2dsZS5jb20vIg%3D%3D; rl_page_init_referring_domain=RS_ENC_v3_Ind3dy5nb29nbGUuY29tIg%3D%3D; rl_session=RS_ENC_v3_eyJpZCI6MTcxNjMwNTE3MTc5MywiZXhwaXJlc0F0IjoxNzE2MzA3MTA0MTc4LCJ0aW1lb3V0IjoxODAwMDAwLCJzZXNzaW9uU3RhcnQiOmZhbHNlLCJhdXRvVHJhY2siOnRydWV9; _wt.mode-2383362=WT3biDSRGyvbyE~; _wt.user-2383362=WT3-rTLRTPNE8SMSeTrW41IUhXOjBnmocQJIgPqcEsh8DOqs8B_Ui0qj0GoyHud0-PbJtuaImDWb3MGb5AylwE1wDivdV_srupxvUi5wmVrslg~",
+        "Referer": "https://flynorse.com/",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+    }
+
+    body = {"PlatformType" : "Web"}
+    token_rsp = requests.post(url, data = json.dumps(body), headers = headers)
+
+    jsn = json.loads(token_rsp.content.decode("utf-8"))
+
+    print(jsn)
+
+    return jsn
+
+def get_fairs(start, end, token, origin, destination):
+    url = "https://services.flynorse.com/api/v1/availability/lowfare"
+    
+    token_str = token['data']['token']
+
+    headers = {
+        "accept": "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Linux\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+        "cookie": "ARRAffinity=aa8e1fc94231a6616d9c9cf9d19a69c2456023250690611bf27c09279050ef5d; ARRAffinitySameSite=aa8e1fc94231a6616d9c9cf9d19a69c2456023250690611bf27c09279050ef5d; _hjSessionUser_3855766=eyJpZCI6IjdjYjE3NTQwLWIzOWMtNTNlZC1hYTA2LTZkODlhYmVjMTlkOCIsImNyZWF0ZWQiOjE3MTYzMDUzMDQwNjUsImV4aXN0aW5nIjp0cnVlfQ==; _hjSession_3855766=eyJpZCI6ImQ3ZjU3MzcwLTg4OWMtNGI0ZS04NzYyLWRkY2NkMDg3ZTk0OCIsImMiOjE3MTYzMDUzMDQwNjYsInMiOjEsInIiOjEsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjoxLCJzcCI6MH0=; FPID=FPID2.2.sa6z3ZtB%2FonPtDqVw%2Beicfh6MQZAccVU6h0LNh92lTo%3D.1716305177; FPLC=coQYWZVI5a5B45hobmS%2FRLx5alIzKdVyU1rJXYj21eHvNQCb0w8%2BIyMeU%2Bud4NT0bdBDAM6YD0EgyExMOC%2Fe0vVtrPxf3LFiQXSHdxTzE%2BoJBenixZnEYIT7VqZAdw%3D%3D; rl_anonymous_id=RS_ENC_v3_IjkwNzc2MDVjLWU5ZDItNDBjZC04NjNmLWMyMmJkYjM1N2Q4YyI%3D; rl_page_init_referrer=RS_ENC_v3_Imh0dHBzOi8vd3d3Lmdvb2dsZS5jb20vIg%3D%3D; rl_page_init_referring_domain=RS_ENC_v3_Ind3dy5nb29nbGUuY29tIg%3D%3D; _wt.mode-2383362=WT3biDSRGyvbyE~; _wt.user-2383362=WT3-rTLRTPNE8SMSeTrW41IUhXOjBnmocQJIgPqcEsh8DOqs8B_Ui0qj0GoyHud0-PbJtuaImDWb3MGb5AylwE1wDivdV_srupxvUi5wmVrslg~;  _clck=1nndw61%7C2%7Cfly%7C0%7C1602; _clsk=1ubgeon%7C1716305307018%7C1%7C1%7Cw.clarity.ms%2Fcollect; _gcl_au=1.1.1186909415.1716305313; _ga=GA1.1.1234196441.1716305313; _scid=708a4ea3-0cf1-4024-b883-3bf672ee1eca; _scid_r=708a4ea3-0cf1-4024-b883-3bf672ee1eca; _ga_LELRJSCXE1=GS1.1.1716305313.1.0.1716305313.60.0.0; _uetsid=663045d0178611efbdae352520153a0c; _uetvid=267561e0182711eeae9a9ba488361b37; _rdt_uuid=1716305313451.1586f309-0f20-4777-ad92-ffba5a191bcc; _ga_XXXXXXXXXX=GS1.1.1716305147.12.1.1716305313.0.0.126804701; mf_8c9a42b5-3bf8-4b15-8edf-911b272553e4=||1716305313484||0||||0|0|94.92248; _tt_enable_cookie=1; _ttp=vgWpWQ7DdRJ0Rt7m_jyRQB8aIZ3; _fbp=fb.1.1716305313636.1436252198; rl_session=RS_ENC_v3_eyJpZCI6MTcxNjMwNTE3MTc5MywiZXhwaXJlc0F0IjoxNzE2MzA3NTg5Njc0LCJ0aW1lb3V0IjoxODAwMDAwLCJzZXNzaW9uU3RhcnQiOmZhbHNlLCJhdXRvVHJhY2siOnRydWV9;",
+        "Referer": "https://flynorse.com/",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
+    } 
+    access_token = "X-Access-Token=" + token_str
+    headers["cookie"] = headers["cookie"] + " " + access_token
+
+    payload = {"childDobs":[],"infantDobs":[],"criteria":[{"origin":"OSL","destination":"BKK","beginDate":start,"endDate":end}],"passengers":[{"type":"ADT","count":1}],"currencyCode":"NOK","promotionCode":"","clearBooking":False}
+
+    r = requests.post(url, data = json.dumps(payload), headers = headers)
+
+    jsn = json.loads(r.content.decode("utf-8"))
+
+
+    jsn["query_date"] = datetime.datetime.now(tz=datetime.timezone.utc)
+    jsn["start"] = start
+    jsn["end"] = end
+
+    return jsn
+
+def main():
     col = connect_mongo()
 
+    token = create_token()
+
+    origin = "OSL"
+    destination = "BKK"
+
+    db_coll = origin + destination
+
+    db_fair = col[db_coll]
+
+    query_time = datetime.datetime.now(tz=datetime.timezone.utc)
     for month in months:
         start = "2024-"+month["start"]
         end = "2024-"+month["end"]
-        payload = {"childDobs":[],"infantDobs":[],"criteria":[{"origin":"OSL","destination":"BKK","beginDate":start,"endDate":end}],"passengers":[{"type":"ADT","count":1}],"currencyCode":"NOK","promotionCode":"","clearBooking":False}
 
-        r = requests.post(url, data = json.dumps(payload), headers = headers)
+        fairs = get_fairs(start, end, token, origin, destination)
 
-        jsn = json.loads(r.content.decode("utf-8"))
-        jsn["query_date"] = datetime.datetime.now(tz=datetime.timezone.utc)
-        jsn["start"] = start
-        jsn["end"] = end
+        if fairs["errors"] != None:
+            print(fairs["errors"])
+            return
 
-        fairs = jsn["data"][0]["cabins"]
-        economy = getFairs(fairs, "Economy")
 
-        print("{} - {}\n".format(start, end))
+        economy_fairs = fairs["data"][0]["cabins"]
 
-        col.insert_one(jsn)
+        economy = getFairs(economy_fairs, "Economy")
+        #premium = getFairs(fairs["data"][0], "Premium")
+
+        db_economy = db_fair["economy"]
         for fare in economy["lowFareAmounts"]:
-            if fare["fareAmount"] != None:
-                sale = "Standard"
-                if fare["isSaleFare"] == True:
-                    sale = "SALE"
+            departure_date = fare["departureDate"]
+            db_departure_date = db_economy[departure_date]
 
-                print("{}  :   {}   {}".format(fare["departureDate"], fare["fareTotal"], sale))
-
-        print("\n")
-
-
-
+            fare["timestamp"] = query_time
+            db_departure_date.insert_one(fare)
+        
 
 
 
